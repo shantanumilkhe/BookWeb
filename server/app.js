f39f9d4e95a54c47be83e6e48f598f57d2152593
Shantanu Milkhe <102807647+shantanumilkhe@users.noreply.github.com>
2023-01-09 14:11:36 +0530

    server connected

---
+++ server/app.js
@@ -0,0 +1,65 @@
+const {google} = require('googleapis');
+const fs = require('fs');
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const methodoverride = require('method-override');
+const session = require('express-session');
+const Admin = require('./models/admin');
+
+const MongoDBStore = require('connect-mongo');
+
+const Dburl = process.env.DB_URL
+mongoose.connect(Dburl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+})
+
+const app = express();
+
+
+const store = new MongoDBStore({
+    mongoUrl: Dburl,
+    secret,
+    touchAfter: 24*3600
+    })
+    
+    store.on("error", function(e){
+        console.log("session Store error", e)
+    })
+    
+    const sessionConfig = {
+        store,
+        name: 'session',
+        secret,
+        resave: false,
+        saveUninitialized: true,
+        cookie: {
+            httpOnly: true,
+            // secure: true,
+            expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+            maxAge: 1000 * 60 * 60 * 24 * 7,
+        }
+    
+    }
+    app.use(session(sessionConfig));
+
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(Admin.authenticate()))
+passport.serializeUser(Admin.serializeUser());
+passport.deserializeUser(Admin.deserializeUser());
+
+
+
+
+  
---
+++ server/routes/drive.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/upload', (req, res) => {
+    // Check if the request is a multi-part request (i.e. it contains a file)
+    if (!req.is('multipart/form-data')) {
+      return res.status(400).send({ message: 'Not a multipart request' });
+    }
+  
+    // Parse the file from the request
+    const file = req.files.file;
+    if (!file) {
+      return res.status(400).send({ message: 'No file was provided' });
+    }
+  
+    // Authenticate with Google Drive
+
+    const oAuth2Client = new google.auth.OAuth2(
+        process.env.CLIENT_ID,
+        process.env.CLIENT_SECRET,
+        process.env.REDIRECT_URI
+    )
+    
+    oAuth2Client.setCredentials({refresh_token: process.env.REFRESH_TOKEN})
+    
+  
+    // Create a new drive instance
+    const drive = google.drive({ version: 'v3', auth: oAuth2Client });
+  
+    // Create a new file on Google Drive
+    drive.files.create({
+      resource: {
+        name: file.name,
+        mimeType: file.mimetype,
+        parents: ['FOLDER_ID'] // optional: put the file in a specific folder
+      },
+      media: {
+        mimeType: file.mimetype,
+        body: file.data
+      }
+    }, (err, result) => {
+      if (err) {
+        return res.status(500).send({ message: err.message });
+      }
+      return res.status(200).send({ message: 'File uploaded successfully', fileId: result.id });
+    });
+  });
+
+  module.exports = router;
