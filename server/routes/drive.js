d323260e40faacf3447f667df1a1378ed69ae898
Shantanu Milkhe <102807647+shantanumilkhe@users.noreply.github.com>
2023-01-15 11:54:18 +0530

    drive.js modified

--- server/routes/drive.js
+++ server/routes/drive.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const {google} = require('googleapis');
+const { google } = require('googleapis');
 const fs = require('fs');
 const path = require('path');
 const chapter = require('../models/book');
@@ -16,13 +16,13 @@ var storage = multer.diskStorage({
   filename: function (req, file, cb) {
 
     let filename = 'filenametogive';
-     req.body.file = filename
+    req.body.file = filename
 
     cb(null, filename)
   }
 })
 
-const upload = multer({ storage: storage})
+const upload = multer({ storage: storage })
 
 
 const oAuth2Client = new google.auth.OAuth2(
@@ -31,83 +31,79 @@ const oAuth2Client = new google.auth.OAuth2(
   process.env.REDIRECT_URI
 )
 
-oAuth2Client.setCredentials({refresh_token: process.env.REFRESH_TOKEN})
+oAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN })
 
 
 // Create a new drive instance
 const drive = google.drive({ version: 'v3', auth: oAuth2Client });
 
-router.post('/upload',upload.single("document"),(req, res) => {
-    console.log(req.file)
-    
-    // Check if the request is a multi-part request (i.e. it contains a file)
-    
-    if (!req.is('multipart/form-data')) {
-      return res.status(400).send({ message: 'Not a multipart request' });
-    }
-  
-    // Parse the file from the request
-    const file = req.file;
-  
-    if (!file) {
-      return res.status(400).send({ message: 'No file was provided' });
-    }
+router.post('/upload', upload.single("document"), (req, res) => {
+  console.log(req.file)
 
-    // Create a new file on Google Drive
-    drive.files.create({
-      resource: {
+  // Check if the request is a multi-part request (i.e. it contains a file)
+
+  if (!req.is('multipart/form-data')) {
+    return res.status(400).send({ message: 'Not a multipart request' });
+  }
+
+  // Parse the file from the request
+  const file = req.file;
+
+  if (!file) {
+    return res.status(400).send({ message: 'No file was provided' });
+  }
+
+  // Create a new file on Google Drive
+  drive.files.create({
+    resource: {
+      name: file.originalname,
+      mimeType: file.mimetype,
+      //parents: ['Chapters'] // optional: put the file in a specific folder
+    },
+    media: {
+      mimeType: file.mimetype,
+      body: fs.createReadStream(file.path)
+    },
+    fields: 'id'
+  }, (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send({ message: err.message });
+    } else {
+      const newFile = new chapter({
+        googleId: result.data.id,
         name: file.originalname,
-        mimeType: file.mimetype,
-        //parents: ['Chapters'] // optional: put the file in a specific folder
-      },
-      media: {
-        mimeType: file.mimetype,
-        body: fs.createReadStream(file.path)
-      },
-      fields: 'id'
-    }, (err, result) => {
-      if (err) {
-        console.log(err);
-        return res.status(500).send({ message: err.message });
-      }else{
-        const newFile = new chapter({
-          googleId: result.data.id,
-          name: file.originalname,
-          size: file.size,
-          type: file.mimetype,
-        });
-        // save the new file object to the database
-        newFile.save()
-        }
-      return res.status(200).send({ message: 'File uploaded successfully', fileId: result.id });
-    });
+        size: file.size,
+        type: file.mimetype,
+      });
+      // save the new file object to the database
+      newFile.save()
+    }
+    return res.status(200).send({ message: 'File uploaded successfully', fileId: result.id });
+  });
+});
+
+router.get('/:id', async (req, res) => {
+  console.log(req.params.id);
+  drive.files.get({
+    fileId: req.params.id,
+    alt: 'media'
+  }, { responseType: 'stream' }, (err, response) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    } else {
+      response.data
+        .on('end', () => {
+          console.log('Done');
+        })
+        .on('error', (err) => {
+          console.log('Error', err);
+        })
+        .pipe(res);
+    }
   });
 
-  router.get('/:id',async (req, res) => {
-    console.log(req.params.id);
-    await drive.files.get({
-      fileId: req.params.id,
-      alt: 'media'
-    }, { responseType: 'stream' }, (err, response) => {
-      if (err) {
-        console.log(err);
-        res.status(500).send(err);
-      } else {
-        // res.set({
-        //   'Content-Disposition': `attachment; filename=${response.data.headers['content-disposition'].match(/"(.*)"/)[1]}`,
-        //   'Content-Type': 'application/pdf'
-        // });
-        response.data
-          .on('end', () => {
-            console.log('Done');
-          })
-          .on('error', (err) => {
-            console.log('Error', err);
-          })
-          .pipe(res);
-      }
-    });
+});
 
-  });
-
-  module.exports = router;
+module.exports = router;
