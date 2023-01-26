9bd68cfb577bf47a95467b83c32d5e813577ae99
Shantanu Milkhe <102807647+shantanumilkhe@users.noreply.github.com>
2023-01-26 18:00:28 +0530

    home page changes

--- client/src/css/CardItem.js
+++ client/src/css/CardItem.js
@@ -13,9 +13,9 @@ function CardItem(props) {
               src={props.src}
             />
           </figure>
-          <div className='cards__item__info'>
+          {/* <div className='cards__item__info'>
             <h5 className='cards__item__text'>{props.text}</h5>
-          </div>
+          </div> */}
         </Link>
       </li>
     </>
