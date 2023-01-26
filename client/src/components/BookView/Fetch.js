8cec4b62811d743c5e819a23c6c87a10cf9c54b3
nikhilnaik5 <82027742+nikhilnaik5@users.noreply.github.com>
2023-01-26 13:07:18 +0530

    Improved viewing experience

--- client/src/components/BookView/Fetch.js
+++ client/src/components/BookView/Fetch.js
@@ -1,90 +1,73 @@
-import React, { useEffect, useState,createRef } from "react";
-import 'react-pdf/dist/esm/Page/TextLayer.css'
-import '../../css/viewer.css'
+import React, { useState, useEffect, createRef } from 'react';
 import { Document, Page } from 'react-pdf';
+import { FixedSizeList } from 'react-window';
+import '../../css/viewer.css'
 const Fetch = (props) => {
-  const [pdfFile, setPdfFile] = useState(null);
+  const listRef = createRef();
   const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
-
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-    setPageNumber(1);
-  }
-
-  function changePage(offset) {
-    setPageNumber(prevPageNumber => prevPageNumber + offset);
-  }
-
-  function previousPage() {
-    changePage(-1);
-  }
-
-  function nextPage() {
-    changePage(1);
-  }
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pdfFile, setPdfFile] = useState(null);
+  const [goto,setGoto] = useState(null);
 
   async function getFile(id) {
     try {
-        const response = await fetch('http://localhost:5000/drive/'+id, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/octet-stream'
-            },
-            responseType: 'blob'
-        });
-        setPdfFile(response.url)
-        const fileBlob = await response.blob();
-        return fileBlob;
+      const response = await fetch('http://localhost:5000/drive/' + id, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/octet-stream'
+        },
+        responseType: 'blob'
+      });
+      setPdfFile(response.url)
+      const fileBlob = await response.blob();
+      return fileBlob;
     } catch (error) {
-        console.error(error);
+      console.error(error);
     }
   }
   useEffect(() => {
     getFile(props.id);
   }, [props.id])
-  
+
+  const renderPage = ({ index, style }) => {
+    return (
+      <div style={style}><Page key={index + 1} pageNumber={index + 1} renderTextLayer={false}/></div>
+    );
+  }
+
   return (
     <div>
-      <div>
-        <p>
-          Page {pageNumber || (numPages ? 1 : '--')} of {numPages || '--'}
-        </p>
-        <button
-          type="button"
-          disabled={pageNumber <= 1}
-          onClick={previousPage}
-        >
-          Previous
-        </button>
-        <button
-          type="button"
-          disabled={pageNumber >= numPages}
-          onClick={nextPage}
-        >
-          Next
-        </button>
-      </div>
-      <div id="ResumeContainer">
-
+      <div>Page no.:{currentPage}/{numPages}</div>
       <Document
-        height='508px'
-        width="100%"
         file={pdfFile}
-        onLoadSuccess={onDocumentLoadSuccess}
+        onLoadSuccess={({ numPages }) => setNumPages(numPages)}
       >
-        <div style={{
-				border: '2px solid lightgray',
-				display: 'flex',
-				height: '100%',
-				borderRadius: '3px',
-			}}>
-        <Page pageNumber={pageNumber} />
-        </div>
+        {numPages && (
+          <>
+            <div>
+              <button onClick={() => { listRef.current.scrollToItem(currentPage - 2)}} disabled={currentPage === 1}>Previous</button>
+              <button onClick={() => { listRef.current.scrollToItem(currentPage)}} disabled={currentPage === numPages}>Next</button>
+              <input type="number" placeholder='Page Number' value={goto} onChange={(e)=>setGoto(Number(e.target.value))} ></input>
+              <button onClick={()=>{listRef.current.scrollToItem(goto-1)}}>Go</button>
+            </div>
+            <FixedSizeList
+              itemCount={numPages}
+              itemSize={800}
+              height={800}
+              width="100%"
+              ref={listRef}
+              onItemsRendered={({ visibleStartIndex, visibleStopIndex }) => {
+                setCurrentPage(visibleStartIndex + 1 )
+              }
+              }
+            >
+              {renderPage}
+            </FixedSizeList>
+          </>
+        )}
       </Document>
-      </div>
     </div>
   );
 }
 
-export default Fetch
+export default Fetch;
