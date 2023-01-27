a057700ddb73a17678f471f85dcaaddfa12db416
Shantanu Milkhe <102807647+shantanumilkhe@users.noreply.github.com>
2023-01-28 00:28:22 +0530

    index page changed

--- client/src/components/BookView/Chapters.js
+++ client/src/components/BookView/Chapters.js
@@ -1,13 +1,21 @@
-import React,{useState,useEffect} from 'react'
-import { useNavigate} from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import '../../css/Chapter.css'
+import '../../css/bookIndex.css'
 const Chapters = () => {
     let navigate = useNavigate();
     const [chapters, setChapter] = useState([]);
+    const [selected, setSelected] = useState(null);
 
-    const pageOpen=(id)=>{
-        let path = '/viewer/'+id;
+    const toogle = (id) => {
+        if (selected === id) {
+            return setSelected(null);
+        }
+        setSelected(id);
+    }
+    const pageOpen = (id) => {
+        let path = '/viewer/' + id;
         navigate(path)
     }
 
@@ -18,32 +26,85 @@ const Chapters = () => {
         getChapters();
     }, [])
 
+
+
     return (
+        // <div className="container">
+        //     {chapters.map((chp, idx) => {
+        //         // return <div>
+        //         //     <div className="card text-white bg-info mb-3" onClick={()=>props.onID(chp.googleId)}>
+        //         //         <div className="card-header">Chapter {idx + 1}</div>
+        //         //         <div className="card-body">
+        //         //             <h5 className="card-title">{chp.name}</h5>
+        //         //         </div>
+        //         //     </div>
+        //         // </div>
+        //         return (
+
+        //             // <div className="card_item">
+        //             //     <div className="card_inner">
+        //             //         <div className="userName">Chapter {idx + 1}</div>
+        //             //         <div className="userUrl">{chp.name}</div>
+        //             //         <div className="detail-box">
+
+        //             //         </div>
+        //             //         <button className="seeMore" onClick={()=>pageOpen(chp.googleId)}>See More</button>
+        //             //     </div>
+        //             // </div>
+
+
+        //         )
+        //     })}
+        // </div>
         <div className="container">
-            {chapters.map((chp, idx) => {
-                // return <div>
-                //     <div className="card text-white bg-info mb-3" onClick={()=>props.onID(chp.googleId)}>
-                //         <div className="card-header">Chapter {idx + 1}</div>
-                //         <div className="card-body">
-                //             <h5 className="card-title">{chp.name}</h5>
-                //         </div>
-                //     </div>
-                // </div>
-                return (
+            <div className="wrapper">
+                <div className="accordion">
+                    {chapters.map((chp, idx) => {
+                        return <div className="item">
 
-                    <div className="card_item">
-                        <div className="card_inner">
-                            <div className="userName">Chapter {idx + 1}</div>
-                            <div className="userUrl">{chp.name}</div>
-                            <div className="detail-box">
-
+                            <div className="title">
+                                <h2>Chapter-{idx + 1}</h2>
+                                <h2>ADMINISTRATION</h2>
+                                {/* <button className="seeMore" onClick={() => pageOpen(chp.googleId)}>See More</button> */}
+                                <span onClick={() => toogle(idx)}>{selected == idx ? '-' : "+"}</span>
                             </div>
-                            <button className="seeMore" onClick={()=>pageOpen(chp.googleId)}>See More</button>
+                            <div className={selected == idx ? 'content show' : "content"}>1.0
+Short Title, Extent and Commencement
+<br />
+1.1 
+Extent and Jurisdiction
+<br />
+1.2
+Commencement of Regulations
+<br />
+1.3 
+Definitions
+<br />
+1.4
+Applicability of Regulations
+<br />
+1.5 
+Saving
+<br />
+1.6
+Applicability of Other Regulations
+<br />
+1.7 *
+Power to Prescribe the Proformas
+<br />
+1.8
+Power to Decide Charges
+<br />
+1.9
+Meanings as in Acts, Rules & Interpretations
+<br />
+1.10
+Removal of Difficulties
+</div>
                         </div>
-
-                    </div>
-                )
-            })}
+                    })}
+                </div>
+            </div>
         </div>
     )
 }
