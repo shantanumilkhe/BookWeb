cf72fbf00791fd0bd605b0f9321f97f1a98f429a
Shantanu Milkhe <102807647+shantanumilkhe@users.noreply.github.com>
2023-02-07 20:18:31 +0530

    css changes

--- client/src/components/admin/Services.js
+++ client/src/components/admin/Services.js
@@ -8,39 +8,43 @@ const Services = () => {
   const [service, setService] = useState(null);
   const axiosInstance = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
-});
+  });
 
   useEffect(() => {
     async function getServiceList() {
-      axiosInstance.get('/sr/allservices').then(res => {setService(res.data.files)});
+      axiosInstance.get('/sr/allservices').then(res => { setService(res.data.files) });
     }
     getServiceList();
   }, []);
 
 
   return (
-  <div>
-    <div className='container'>
-      <div className="btn" onClick={() => navigate('/admin/newservice')}>
-        <p className="card-text">Add a new service</p>
-      </div>
-      <div className="cards-container">
-        {service ? service.map((post) => {
-          return (
-            <div className="c-card" onClick={() => navigate('/admin/editservice/' + post.id)}>
-              <img classname="cardImage" src={post.images} />
-              <div className="c-card__content">
-                <h1>{post.name}</h1>
-                <p >{post.description}</p>
+    <div>
+      <div className='container'>
+        <div className="btn" onClick={() => navigate('/admin/newservice')}>
+          <p className="card-text">Add a new service</p>
+        </div>
+        <div className="cards-container">
+          {service ? service.map((post) => {
+            return (
+              <div className="c-card" onClick={() => navigate('/admin/editservice/' + post.id)}>
+                <img classname="cardImage" src={post.images} />
+                <div className="c-card__content">
+                  <h1>{post.name}</h1>
+                  <div className="ll">
+                    <p >{post.description}</p>
+                    <span className="read">Read More</span>
+                  </div>
 
+
+                </div>
               </div>
-            </div>
-          )
-        }) : null}
+            )
+          }) : null}
+        </div>
       </div>
-    </div>
     </div>
   )
 }
 
-    export default Services;
+export default Services;
