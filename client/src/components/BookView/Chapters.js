b6cf2b9cafcc7293f05aa891c348132089dfca97
Shantanu Milkhe <102807647+shantanumilkhe@users.noreply.github.com>
2023-02-28 15:35:05 +0530

    UI Changes

--- client/src/components/BookView/Chapters.js
+++ client/src/components/BookView/Chapters.js
@@ -5,14 +5,15 @@ import '../../css/Chapter.css'
 import '../../css/bookIndex.css'
 
 function romanize(num) {
-    var lookup = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1},roman = '',i;
-    for ( i in lookup ) {
-      while ( num >= lookup[i] ) {
-        roman += i;
-        num -= lookup[i];
-      }
+    if(num<0){
+       if(num==-2) return "I";
+       else if(num==-1) return "II";
+       else return Math.abs(num);
+       
     }
-    return roman;
+    else{
+        return num;
+    }
   }
   
 const Chapters = () => {
